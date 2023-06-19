627bd58770ae5830eff152c3b8eb40ec9c0b2e56
kipmaiyo <danielkipmaiyo44@gmail.com>
2023-06-19 14:23:36 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Write your solution here!
+// var kittens = [] //define your array here
+
+
+let cats = ['Milo','Otis','Garfield'] 
+  
+  function catsLengthAndOrder (cats) {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  };
+
+// Add your functions and code here
+
+function destructivelyAppendCat(name) {
+    cats.push('Ralph');
+   return cats;
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+  return cats;
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+  return cats
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+  return cats;
+}
+
+function appendCat(name){
+  let catName= [...cats,"Broom"];
+  return catName;
+}
+
+function prependCat(name){
+  let catsname_2 =["Arnold", ...cats];
+  return catsname_2
+}
+
+function removeLastCat(){
+  let caats= cats.slice(0, cats.length-1);
+  return caats;
+}
+
+function removeFirstCat(){
+  let catts= cats.slice(1)
+  return catts
+}
